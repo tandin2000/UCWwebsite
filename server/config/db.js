9901b499bf84b943c40741a3bb72742ec2666552
Tandin Wangchen <taandinwangchen27@icloud.com>
2025-06-13 13:16:21 -0700

    chnages

--- server/config/db.js
+++ server/config/db.js
@@ -5,8 +5,9 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
+  database: process.env.DB_NAME,   // add this
   waitForConnections: true,
-  connectionLimit: 10,
+  connectionLimit: 10000,
   queueLimit: 0
 });
 
